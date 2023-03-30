54589f81752a5abe01ed1e14ac2dfa1f206e1c6a
John Mark Victorino <victorinojohnmark@gmail.com>
2023-03-30 09:03:20 +0800

    JMV update customer with invoice

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 
 use App\Filters\V1\CustomersFilter;
+use App\Http\Requests\V1\StoreCustomerRequest;
 
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
@@ -26,22 +26,15 @@ class CustomerController extends Controller
         $filterItems = $filter->transform($request); //[['column', 'operator', 'value']]
 
         //Note: tuts video timestamp 1:00:00
-        $includeInvoices = $request->query('include');
+        $includeInvoices = $request->query('includeInvoices');
 
-        if(count($filterItems) == 0) {
-            return new CustomerCollection(Customer::paginate());
-        } else {
-            $customers = Customer::where($filterItems)->paginate();
-            return new CustomerCollection($customers->appends($request->query()));
+        $customers = Customer::where($filterItems);
+
+        if($includeInvoices) {
+            $customers = $customers->with('invoices');
         }
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
     }
 
     /**
@@ -49,23 +42,20 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Customer $customer)
+    public function show(Customer $customer, Request $request)
     {
-        return new CustomerResource($customer);
-    }
+        $includeInvoices = $request->query('includeInvoices');
+        if($includeInvoices) {
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Customer $customer)
-    {
-        //
+        return new CustomerResource($customer);
     }
 
     /**
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,16 @@ class Customer extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'type',
+        'email',
+        'address',
+        'city',
+        'state',
+        'postalCode'
+    ];
+
     public function invoices() {
         return $this->hasMany(Invoice::class);
     }
